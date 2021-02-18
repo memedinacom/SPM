9cbdaa21bb565d9797be70eef51ca30f8b79d815
Mehmet Zeytin <mzeytin@protel.com.tr>
2021-02-18 23:31:59 +0300

    -spm fix

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SPM",
-            dependencies: []),
+            dependencies: ["Alamofire","Kingfisher" ,"Lottie", "Realm" ,"RealmDatabase"]),
         .testTarget(
             name: "SPMTests",
             dependencies: ["SPM"]),
